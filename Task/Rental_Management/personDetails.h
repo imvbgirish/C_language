6dd1229484b17297da3a66bfbf512695cce075a6
imvbgirish <girishvb26@gmail.com>
2025-04-06 19:10:07 +0530

    Addedd delete function and updated other functions

--- Task/Rental_Management/personDetails.h
+++ Task/Rental_Management/personDetails.h
@@ -3,15 +3,11 @@
 
 typedef struct personDetails{
     char name[10];
-    int contactNumber;
+    char contactNumber[15];
     char emailID[20];
-    int password;
+    char password[15];
     char isAdmin[5];
 }person;
 
-enum isAdmin {
-  Yes,
-    No
-};
 
 #endif // PERSONDETAILS_H
