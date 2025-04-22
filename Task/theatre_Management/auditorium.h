983f704e69e52915206a06de735369b1a11f1b76
imvbgirish <girishvb26@gmail.com>
2025-04-22 09:48:48 +0530

    Created theatre management project

---
+++ Task/theatre_Management/auditorium.h
@@ -0,0 +1,11 @@
+#ifndef AUDITORIUM_H
+#define AUDITORIUM_H
+
+#include "calender.h"
+
+typedef struct{
+    char auditoriumName[10];
+    calender *calender;
+}auditorium;
+
+#endif // AUDITORIUM_H
